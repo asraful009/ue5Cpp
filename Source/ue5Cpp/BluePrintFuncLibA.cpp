d831521cd5199c2da890d8b0c4eaabf0d46f6cc3
asraful islam <asraful009@gmail.com>
2023-11-27 17:43:09 +0600

    Calc Lib

--- Source/ue5Cpp/BluePrintFuncLibA.cpp
+++ Source/ue5Cpp/BluePrintFuncLibA.cpp
@@ -3,7 +3,64 @@
 
 #include "BluePrintFuncLibA.h"
 
+#include "Algo/Accumulate.h"
+
 FString UBluePrintFuncLibA::GetMeg()
 {
-	return FString(TEXT("Hi All"));
+	return FString(TEXT("Hi All."));
+}
+
+double UBluePrintFuncLibA::GetPi()
+{
+	return Pi;	
+}
+
+FAverages UBluePrintFuncLibA::CalcAverages(const TArray<float>& Values)
+{	
+	TArray<float> TempValues = Values;
+	TMap<float, int32> ModelMap;
+	
+	const int32 Total = TempValues.Num();	
+	if(Total == 0) return FAverages();
+	
+	TempValues.Sort();
+	
+	float Sum = 0.0f; //Algo::Accumulate(TempValues, 0.0f);
+	for(int32 i = 0; i<Total; i++)
+	{
+		Sum += TempValues[i];
+		if(!ModelMap.Contains(TempValues[i]))
+		{
+			ModelMap.Add(TempValues[i], 0);	
+		}
+		ModelMap[TempValues[i]]++;
+	}
+	const float Mean = Sum / static_cast<float>(Total);
+	const float Median =
+		Total % 2 == 0 ?
+			TempValues[Total/2] :
+			static_cast<float>( TempValues[Total / 2] + TempValues[Total / 2 - 1] ) / 2.0;
+	ModelMap.ValueSort([](const int32 A, const int32 B) { return A > B;});
+	
+	TArray<TPair<float, int32>> ModelPair = ModelMap.Array();
+	float Model = 0.0;
+	if(ModelPair[0].Value == 1)
+	{
+		Model = Mean;
+	}
+	else
+	{
+		int32 ModelEntries = 1;
+		int32 ModelSum = ModelPair[0].Key;
+
+		for(int32 i = 0; i < ModelPair.Num(); i++)
+		{
+			if(ModelPair[i].Value != ModelPair[0].Value) break;
+			ModelSum += ModelPair[i].Key;
+			ModelEntries++;
+		}
+		Model = static_cast<float>(ModelSum) / static_cast<float>(ModelEntries);
+		
+	}
+	return  FAverages(Mean, Model, Median);
 }
--- Source/ue5Cpp/BluePrintFuncLibA.h
+++ Source/ue5Cpp/BluePrintFuncLibA.h
@@ -6,13 +6,49 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "BluePrintFuncLibA.generated.h"
 
+USTRUCT(BlueprintType)
+struct FAverages
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	float Mean = 0.0f;
+	UPROPERTY(BlueprintReadOnly)
+	float Model = 0.0f;
+	UPROPERTY(BlueprintReadOnly)
+	float Median = 0.0f;
+	
+	FAverages():
+		Mean(0.0f),
+		Model(0.0f),
+		Median(0.0f) {}
+	
+	FAverages(const float Mean, const float Model, const float Median):
+		Mean(Mean),
+		Model(Model),
+		Median(Median) {}
+	
+};
+
 /**
  * 
  */
 UCLASS()
 class UE5CPP_API UBluePrintFuncLibA : public UBlueprintFunctionLibrary
 {
+	
 	GENERATED_BODY()
 
+	UFUNCTION(BlueprintCallable)
 	static FString GetMeg();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure,
+		meta=(ToolTip = "3.124..", CompactNodeTitle = "π"))
+	static double GetPi();
+
+	UFUNCTION(BlueprintCallable, meta=(ToolTip = "CalcAverages", CompactNodeTitle = "μ"))
+	static FAverages CalcAverages(const TArray<float>& Values);
+	
+private:
+	static constexpr double Pi = 3.141592653589793238462643383279502884197;
 };
