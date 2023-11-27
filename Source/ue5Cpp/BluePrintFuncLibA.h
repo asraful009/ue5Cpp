67654d7c9a66564c96c7801e6ece3a12a2d37012
asraful islam <asraful009@gmail.com>
2023-11-27 13:14:04 +0600

    init c++

---
+++ Source/ue5Cpp/BluePrintFuncLibA.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BluePrintFuncLibA.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5CPP_API UBluePrintFuncLibA : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
